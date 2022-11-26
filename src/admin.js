b9527743cce6bab24c0e28ab58ac77d16ee515b7
osmomysl <dannvx@gmail.com>
2022-11-26 21:05:18 +0200

    use extracted api for network requests

--- src/admin.js
+++ src/admin.js
@@ -2,109 +2,107 @@ import React, {Component, useState, useEffect} from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Routes, Route, Link, Navigate, useParams, useNavigate } from "react-router-dom" // , useNavigate
 
-import {Signup, Login} from './admin/auth.js'
+import auth from './admin/auth.js'
 
-function useIsLoggedIn() {
-    const [isLoggedIn, setIsLoggedIn] = useState(null)
+function main(container, api) {
+    const {Signup, Login} = auth(api)
 
-    useEffect(() => {
-        fetch('/api/admin/auth/is-authenticated', {method: 'GET'}).then((res) => {
-            res.json().then((body) => {
-                setIsLoggedIn(body)
-            })
-        })
-    }, [])
-
-    return isLoggedIn
-}
-
-function Logout() {
-    const navigate = useNavigate()
-
-    useEffect(() => {
-        fetch('/api/admin/user/logout', {method: 'GET'}).then(res => {
-            if (!res.ok) {
-                console.log("logout response not ok - res:", res);
-                return alert("something is wrong with the program, please consult a technician")
-            }
-
-            navigate('/')
-        })
-    }, [])
-
-    return (<div>{'logging out'}</div>)
-}
-
-function Dash(props) {
-    const navigate = useNavigate()
-
-    const isLoggedIn = useIsLoggedIn()
+    function useIsLoggedIn() {
+        const [isLoggedIn, setIsLoggedIn] = useState(null)
     
-    useEffect(() => {
-        if (null === isLoggedIn) return
-
-        if (!isLoggedIn) navigate('/signin')
-    }, [isLoggedIn])
-
-    return (
-        isLoggedIn 
+        useEffect(() => {
+            api.isAuthenticated((body) => {
+                setIsLoggedIn(body)
+            }, () => {})
+        }, [])
+    
+        return isLoggedIn
+    }
+    
+    function Logout() {
+        const navigate = useNavigate()
+    
+        useEffect(() => {
+            api.logout(() => {
+                navigate('/')
+            }, (body, res) => {
+                console.log("logout response not ok - res, body:", res, body);
+                return alert("something is wrong with the program, please consult a technician")
+            })
+        }, [])
+    
+        return (<div>{'logging out'}</div>)
+    }
+    
+    function Dash(props) {
+        const navigate = useNavigate()
+    
+        const isLoggedIn = useIsLoggedIn()
         
-        ?  
-        <div className="admin">
-            <nav>
-                <Link to="orders">orders</Link>
-                <Link to="products">products</Link>
-                <Link to="product">product</Link>
-                <Link to="logout">logout</Link>
-            </nav>
-            <section>
+        useEffect(() => {
+            if (null === isLoggedIn) return
+    
+            if (!isLoggedIn) navigate('/signin')
+        }, [isLoggedIn])
+    
+        return (
+            isLoggedIn 
+            
+            ?  
+            <div className="admin">
+                <nav>
+                    <Link to="orders">orders</Link>
+                    <Link to="products">products</Link>
+                    <Link to="product">product</Link>
+                    <Link to="logout">logout</Link>
+                </nav>
+                <section>
+                    <Routes>
+                        <Route index element={<Navigate to="orders"/>}></Route>
+                        <Route path="orders" element={<div className="orders">orders</div>} />
+                        <Route path="products" element={<div className="products">products</div>} />
+                        <Route path="product" element={<div className="product-create">create product</div>} />
+                        <Route path="product/:id" element={<div className="product">product</div>} />
+                        <Route path="logout" element={<Logout />}/>
+                    </Routes>
+                </section>
+            </div>
+            
+            : 
+            null
+        )
+    }
+    
+    function Signin(props) {
+        return (
+            <div>
+                <Link to="/login">{'login'}</Link>
+                <Link to="/signup">{'signup'}</Link>
+            </div>
+        )
+    }
+    
+    function Blank(props) {
+        return (<div>{"route doesn't exist"}</div>)
+    }
+    
+    function App(props) {
+        return (
+            <div className="app">
                 <Routes>
-                    <Route index element={<Navigate to="orders"/>}></Route>
-                    <Route path="orders" element={<div className="orders">orders</div>} />
-                    <Route path="products" element={<div className="products">products</div>} />
-                    <Route path="product" element={<div className="product-create">create product</div>} />
-                    <Route path="product/:id" element={<div className="product">product</div>} />
-                    <Route path="logout" element={<Logout />}/>
+                    <Route path="/">
+                        <Route index element={<Navigate to="dash"/>} />
+                        <Route path="dash/*" element={<Dash />} />
+                        <Route path="signin" element={<Signin />}/>
+                        <Route path="login" element={<Login />} />
+                        <Route path="signup" element={<Signup />} />
+                    </Route>
+                    <Route path="/*" element={<Blank />} />
                 </Routes>
-            </section>
-        </div>
-        
-        : 
-        null
-    )
-}
+            </div>
+        )
+    }
 
-function Signin(props) {
-    return (
-        <div>
-            <Link to="/login">{'login'}</Link>
-            <Link to="/signup">{'signup'}</Link>
-        </div>
-    )
-}
-
-function Blank(props) {
-    return (<div>{"route doesn't exist"}</div>)
-}
-
-function App(props) {
-    return (
-        <div className="app">
-            <Routes>
-                <Route path="/">
-                    <Route index element={<Navigate to="dash"/>} />
-                    <Route path="dash/*" element={<Dash />} />
-                    <Route path="signin" element={<Signin />}/>
-                    <Route path="login" element={<Login />} />
-                    <Route path="signup" element={<Signup />} />
-                </Route>
-                <Route path="/*" element={<Blank />} />
-            </Routes>
-        </div>
-    )
-}
-
-function main(container) {
     const root = ReactDOM.createRoot(container)
     root.render(<BrowserRouter basename="/admin"><App /></BrowserRouter>)
 }
--- src/admin/auth.js
+++ src/admin/auth.js
@@ -1,181 +1,133 @@
 import * as m from '../../../fi-common/messages.js'
 
-// import {useState} from "react"
 import React, {Component, useState} from "react"
-// import ReactDOM from "react-dom"
 import { useNavigate } from "react-router-dom"
 
-function signup(name, password, successCb, failureCb) {
-    const body = new URLSearchParams()
-    body.append('name', name)
-    body.append('password', password)
-
-    const req = new Request('/api/admin/auth/signup', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: body.toString()
-    })
-
-    return fetch(req).then((res) => {
-        // https://github.com/facebook/react-native/issues/6679
-        setTimeout(() => null, 0)
-        return res.json().then(body => {return {res, body}})
-    }).then(({res, body}) => {
-        res.ok ? successCb({res, body}, name, password) : failureCb({res, body}, name, password)
-    })
-}
-
-function login(name, password, successCb, failureCb) {
-    const body = new URLSearchParams()
-    body.append('name', name)
-    body.append('password', password)
-
-    const req = new Request('/api/admin/auth/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: body.toString()
-    })
-
-    return fetch(req).then((res) => {
-        // https://github.com/facebook/react-native/issues/6679
-        setTimeout(() => null, 0)
-        return res.json().then(body => {return {res, body}})
-    }).then(({res, body}) => {
-        res.ok ? successCb({res, body}, name, password) : failureCb({res, body}, name, password)
-    })
-}
-
-function Signup(props) {
-    const [name, setName] = useState('a')
-    const [password, setPassword] = useState('a')
-    const [nameMsgs, setNameMsgs] = useState([]);
-    const [passwordMsgs, setPasswordMsgs] = useState([]);
-    const [msg, setMsg] = useState('')
-
-    const navigate = useNavigate()
-
-    const clickCb = () => {
-        signup(name, password, ({res, body}, name, password) => {
-            login(name, password, ({res, body}) => {
-                return navigate('../')
-            }, ({res, body}) => {
-                // console.log('Signup, login response, body:', res, body)
-                
+function main(api) {
+    function Signup(props) {
+        const navigate = useNavigate()
+        
+        const [name, setName] = useState('a')
+        const [password, setPassword] = useState('a')
+        const [nameMsgs, setNameMsgs] = useState([]);
+        const [passwordMsgs, setPasswordMsgs] = useState([]);
+        const [msg, setMsg] = useState('')
+    
+        const clickCb = () => {
+            api.signup(name, password, (body, res, name, password) => {
+                api.login(name, password, (body, res) => {
+                    return navigate('../')
+                }, (body, res) => {
+                    // console.log('Signup, login response, body:', res, body)
+                    
+                    if (res.status >= 500) {
+                        console.log(`signup, internal error when logging in - res, body`, res, body)
+                        return alert(`Something's wrong on the server, please consult a technician`)
+                    }
+    
+                    if (!body.message) {
+                        console.log(`signup, response not ok when logging in - res, body:`, res, body);
+                        return setMsg(`Some fields are filled incorrectly`)
+                    }
+    
+                    setMsg(body.message)
+                })
+            }, (body, res) => {
+                // console.log('Signup, fetch res:', res, body);
+    
                 if (res.status >= 500) {
-                    console.log(`signup, internal error when logging in - res, body`, res, body)
+                    console.log(`signup, internal error - res, body`, res, body)
                     return alert(`Something's wrong on the server, please consult a technician`)
                 }
-
+    
+                if (body.tree) {
+                    if (body.tree.node.name) setNameMsgs([...body.tree.node.name.errors.reduce((msgs, e) => {
+                        if (e.message) msgs.push(e.message)
+                    }, [])])
+    
+                    if (body.tree.node.password) setPasswordMsgs([...body.tree.node.password.errors.reduce((msgs, e) => {
+                        if (e.message) msgs.push(e.message)
+                    }, [])])
+    
+                    return
+                }
+    
                 if (!body.message) {
-                    console.log(`signup, response not ok when logging in - res, body:`, res, body);
+                    console.log(`signup, response not ok - res, body:`, res, body);
                     return setMsg(`Some fields are filled incorrectly`)
                 }
-
+    
+                setMsg(body.message)
+            })
+        }
+    
+        return (
+            <form onSubmit={e => e.preventDefault()} id="signup">
+                <input onInput={(ev) => {setName(ev.target.value); setNameMsgs([])}} value={name} type="text" name="name"/>
+                <p>{nameMsgs.map(v => <span>{v}</span>)}</p>
+                <input onInput={(ev) => {setPassword(ev.target.value); setPasswordMsgs([])}} value={password} type="text" name="password"/>
+                <p>{passwordMsgs.map(v => <span>{v}</span>)}</p>
+                <p><span>{msg}</span></p>
+                <button onClick={clickCb}></button>
+            </form>
+        )
+    }
+    
+    function Login(props) {
+        const [name, setName] = useState('a')
+        const [password, setPassword] = useState('a')
+        const [nameMsgs, setNameMsgs] = useState([])
+        const [passwordMsgs, setPasswordMsgs] = useState([])
+        const [msg, setMsg] = useState('')
+    
+        const navigate = useNavigate()
+    
+        const clickCb = () => {
+            api.login(name, password, (body, res) => {
+                return navigate('../')
+            }, (body, res) => {
+                if (res.status >= 500) {
+                    console.log(`signup, internal error - res, body`, res, body)
+                    return alert(`Something's wrong on the server, please consult a technician`)
+                }
+    
+                if (body.tree) {
+                    if (body.tree.node.name) setNameMsgs([...body.tree.node.name.errors.reduce((msgs, e) => {
+                        if (e.message) msgs.push(e.message)
+                    }, [])])
+    
+                    if (body.tree.node.password) setPasswordMsgs([...body.tree.node.password.errors.reduce((msgs, e) => {
+                        if (e.message) msgs.push(e.message)
+                    }, [])])
+    
+                    return
+                }
+    
+                if (!body.message) {
+                    console.log(`signup, response not ok - res, body:`, res, body);
+                    return setMsg(`Some fields are filled incorrectly`)
+                }
+    
                 setMsg(body.message)
             }).catch((e) => {
-                console.log('signup, login rejected - error:', e)
+                console.log('login rejected - error:', e)
                 alert(`Something is wrong with the program, please consult a technician`)
             })
-        }, ({res, body}, name, password) => {
-            // console.log('Signup, fetch res:', res, body);
-
-            if (res.status >= 500) {
-                console.log(`signup, internal error - res, body`, res, body)
-                return alert(`Something's wrong on the server, please consult a technician`)
-            }
-
-            if (body.tree) {
-                if (body.tree.node.name) setNameMsgs([...body.tree.node.name.errors.reduce((msgs, e) => {
-                    if (e.message) msgs.push(e.message)
-                }, [])])
-
-                if (body.tree.node.password) setPasswordMsgs([...body.tree.node.password.errors.reduce((msgs, e) => {
-                    if (e.message) msgs.push(e.message)
-                }, [])])
-
-                return
-            }
-
-            if (!body.message) {
-                console.log(`signup, response not ok - res, body:`, res, body);
-                return setMsg(`Some fields are filled incorrectly`)
-            }
-
-            setMsg(body.message)
-        }).catch((e) => {
-            console.log('signup rejected - error:', e)
-            alert(`Something is wrong with the program, please consult a technician`)
-        })
+        }
+    
+        return (
+            <form onSubmit={e => e.preventDefault()} id="signup">
+                <input onInput={(ev) => {setName(ev.target.value)}} value={name} type="text" name="name"/>
+                <p>{nameMsgs.map(v => <span>{v}</span>)}</p>
+                <input onInput={(ev) => {setPassword(ev.target.value)}} value={password} type="text" name="password"/>
+                <p>{passwordMsgs.map(v => <span>{v}</span>)}</p>
+                <p><span>{msg}</span></p>
+                <button onClick={clickCb}></button>
+            </form>
+        )
     }
 
-    return (
-        <form onSubmit={e => e.preventDefault()} id="signup">
-            <input onInput={(ev) => {setName(ev.target.value); setNameMsgs([])}} value={name} type="text" name="name"/>
-            <p>{nameMsgs.map(v => <span>{v}</span>)}</p>
-            <input onInput={(ev) => {setPassword(ev.target.value); setPasswordMsgs([])}} value={password} type="text" name="password"/>
-            <p>{passwordMsgs.map(v => <span>{v}</span>)}</p>
-            <p><span>{msg}</span></p>
-            <button onClick={clickCb}></button>
-        </form>
-    )
+    return {Signup, Login}
 }
 
-function Login(props) {
-    const [name, setName] = useState('a')
-    const [password, setPassword] = useState('a')
-    const [nameMsgs, setNameMsgs] = useState([])
-    const [passwordMsgs, setPasswordMsgs] = useState([])
-    const [msg, setMsg] = useState('')
-
-    const navigate = useNavigate()
-
-    const clickCb = () => {
-        login(name, password, ({res, body}, name, password) => {
-            return navigate('../')
-        }, ({res, body}, name, password) => {
-            if (res.status >= 500) {
-                console.log(`signup, internal error - res, body`, res, body)
-                return alert(`Something's wrong on the server, please consult a technician`)
-            }
-
-            if (body.tree) {
-                if (body.tree.node.name) setNameMsgs([...body.tree.node.name.errors.reduce((msgs, e) => {
-                    if (e.message) msgs.push(e.message)
-                }, [])])
-
-                if (body.tree.node.password) setPasswordMsgs([...body.tree.node.password.errors.reduce((msgs, e) => {
-                    if (e.message) msgs.push(e.message)
-                }, [])])
-
-                return
-            }
-
-            if (!body.message) {
-                console.log(`signup, response not ok - res, body:`, res, body);
-                return setMsg(`Some fields are filled incorrectly`)
-            }
-
-            setMsg(body.message)
-        }).catch((e) => {
-            console.log('login rejected - error:', e)
-            alert(`Something is wrong with the program, please consult a technician`)
-        })
-    }
-
-    return (
-        <form onSubmit={e => e.preventDefault()} id="signup">
-            <input onInput={(ev) => {setName(ev.target.value)}} value={name} type="text" name="name"/>
-            <p>{nameMsgs.map(v => <span>{v}</span>)}</p>
-            <input onInput={(ev) => {setPassword(ev.target.value)}} value={password} type="text" name="password"/>
-            <p>{passwordMsgs.map(v => <span>{v}</span>)}</p>
-            <p><span>{msg}</span></p>
-            <button onClick={clickCb}></button>
-        </form>
-    )
-}
-
-export {Signup, Login}
+export default main
