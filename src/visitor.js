4b1100855b59b665096a9df6f6a59832f6c47879
osmomysl <dannvx@gmail.com>
2023-06-02 22:50:37 +0300

    visitor: add React

--- src/visitor.js
+++ src/visitor.js
@@ -1,9 +1,19 @@
-function main(api) {
-    api.product.getMany('name', 1, false, (body) => {
-        console.log('api.product.getMany, successCb - body:', body)
-    }, () => {
-        console.log('api.product.getMany, failureCb - body:', body)
-    })
+import React, {Component, useState, useEffect} from "react"
+import ReactDOM from "react-dom/client"
+
+function main(container, api) {
+    function App() {
+        api.product.getMany('name', 1, false, (body) => {
+            console.log('api.product.getMany, successCb - body:', body)
+        }, () => {
+            console.log('api.product.getMany, failureCb - body:', body)
+        })
+
+        return <div></div>
+    }
+
+    const root = ReactDOM.createRoot(container)
+    root.render(<App />)
 }
 
 export default main
