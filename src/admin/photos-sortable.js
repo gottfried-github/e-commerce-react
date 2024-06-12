8af31bc9c98398c9f0d995135fb00da9910e9a7e
gottfried <gottfried.dev@gmail.com>
2024-06-12 20:30:58 +0300

    implement reordering photos laid out in a grid

--- src/admin/photos-sortable.js
+++ src/admin/photos-sortable.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useDrag, useDrop } from 'react-dnd'
 
 const DndTypes = {
@@ -11,22 +11,53 @@ const DndTypes = {
  * @description uses react-dnd to reorder rendered photos
  */
 function PhotosSortable({ photos, reorderCb }) {
-  const dropCb = (target, source) => {
-    const _photos = [...photos]
+  const [_photos, setPhotos] = useState(photos)
 
-    const tI = _photos.map(photo => photo.id).indexOf(target)
-    const sI = _photos.map(photo => photo.id).indexOf(source)
+  useEffect(() => {
+    setPhotos(photos)
+  }, [photos])
 
-    // insert source before target
-    _photos.splice(tI, 0, _photos.splice(sI, 1)[0])
+  const hoverCb = (target, source, clientOffset) => {
+    if (target.id === source.id) return
 
+    const __photos = [..._photos]
+
+    const tI = __photos.map(photo => photo.id).indexOf(target.id)
+    const sI = __photos.map(photo => photo.id).indexOf(source.id)
+
+    const targetClientRect = target.ref.current.getBoundingClientRect()
+    const middle = targetClientRect.width / 2 + targetClientRect.x
+
+    if (clientOffset.x <= middle) {
+      // insert source before target
+      __photos.splice(tI, 0, __photos.splice(sI, 1)[0])
+    } else {
+      // insert source after target
+      __photos.splice(tI + 1, 0, __photos.splice(sI, 1)[0])
+    }
+
+    setPhotos(__photos)
+  }
+
+  const dragCancelCb = source => {
+    setPhotos(photos)
+  }
+
+  const dropCb = (target, source, clientOffset) => {
     reorderCb(_photos)
   }
 
   return (
     <div className="photos">
-      {photos.map(photo => (
-        <PhotoSortable key={photo.id} id={photo.id} photo={photo} dropCb={dropCb} />
+      {_photos.map(photo => (
+        <PhotoSortable
+          key={photo.id}
+          id={photo.id}
+          photo={photo}
+          dropCb={dropCb}
+          hoverCb={hoverCb}
+          dragCancelCb={dragCancelCb}
+        />
       ))}
     </div>
   )
@@ -38,23 +69,29 @@ function PhotosSortable({ photos, reorderCb }) {
  * @param {Function} dropCb callback to fire when a photo is dropped on the photo
  * @description uses react-dnd to implement draggable and droppable photos
  */
-function PhotoSortable({ id, photo, dropCb }) {
+function PhotoSortable({ id, photo, dropCb, hoverCb, dragCancelCb }) {
+  const ref = useRef()
+
   const [collectedDrop, drop] = useDrop({
     accept: DndTypes.item,
-    // hover: (item, monitor) => {
-    //     console.log('Item, hover - item:', item)
-    // },
+    hover: (item, monitor) => {
+      hoverCb({ id, ref }, item, monitor.getClientOffset())
+    },
     drop: (item, monitor) => {
-      dropCb(id, item.id)
+      dropCb({ id, ref }, item, monitor.getClientOffset())
     },
   })
 
   const [collectedDrag, drag, dragPreview] = useDrag(() => ({
     type: DndTypes.item,
-    item: { id },
+    item: { id, ref },
+    end: (item, monitor) => {
+      if (!monitor.didDrop()) {
+        dragCancelCb(item)
+      }
+    },
   }))
 
-  const ref = useRef()
   drag(drop(ref))
 
   return (
