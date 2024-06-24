bb7164ecc4cd95dec00e2e409ce66a9c9eea5bf9
gottfried <gottfried.dev@gmail.com>
2024-06-24 14:29:55 +0300

    wip, PhotosDrawer: add layout

---
+++ src/admin/photo-drawer-item.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Paper from '@mui/material/Paper/index.js'
+import Divider from '@mui/material/Divider/index.js'
+import Button from '@mui/material/Button/index.js'
+import Checkbox from '@mui/material/Checkbox/index.js'
+import Radio from '@mui/material/Radio/index.js'
+import FormControlLabel from '@mui/material/FormControlLabel/index.js'
+
+const PhotosDrawerItem = ({ photo, handlePublicPick, handleCoverPick, handleRemove, disabled }) => {
+  return (
+    <Paper elevation={3}>
+      <div className="photos-drawer__photo">
+        <img src={photo.pathPublic} alt="photo drawer photo" />
+      </div>
+      <div className="photos-drawer__content-box">
+        <FormControlLabel
+          control={<Checkbox checked={photo.public} />}
+          label="Показувати відвідувачам"
+          disabled={disabled}
+        />
+        <FormControlLabel
+          control={<Radio checked={photo.cover} name="photo-cover" />}
+          label="Поставити на обкладинку"
+          disabled={disabled}
+        />
+      </div>
+      <Divider />
+      <div className="photos-drawer__content-box">
+        <div className="flex-justify-end">
+          <Button variant="contained" color="error">
+            Видалити
+          </Button>
+        </div>
+      </div>
+    </Paper>
+  )
+}
+
+export default PhotosDrawerItem
